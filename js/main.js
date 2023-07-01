cdb51c10a6f6932cb014d6c08563d178f7cbb7db
cartrugs <cartrugs@gmail.com>
2023-07-01 16:41:52 +0200

    Desarrollo miniaturas js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 /* Variables y arrays */
 
+const body = document.querySelector('body');
+
+const miniaturas = document.querySelector('#miniaturas');
+const miniaturasArray = [
+    {
+        url: 'assets/images/viajes-1.jpg',
+        alt: 'Chica sobre hamaca en la playa',
+    },
+    {
+        url: 'assets/images/viajes-6.jpg',
+        alt: 'Paseo marítimo con tunel natural',
+    },
+    {
+        url: 'assets/images/viajes-3.jpg',
+        alt: 'Pueblo rural con castillo en el monte',
+    }
+];
+const miniFragmentAnadir = document.createDocumentFragment();
+
 /* Eventos */
 
 /* Funciones */
+
+const cajasMiniaturas = () => {
+    const caja = document.createElement('DIV');
+    const imagenes = document.createElement('IMG');
+    imagenes.src = imagenes.url, imagenes.alt;
+    caja.append(imagenes);
+};
+
+cajasMiniaturas();
