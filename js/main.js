18aacfb1daa80eec4c4001161bcbb9a5135814cd
cartrugs <cartrugs@gmail.com>
2023-07-02 18:19:48 +0200

    Desarrollo de evento mostrar y ocultar (Carlos)

--- js/main.js
+++ js/main.js
@@ -23,36 +23,48 @@ const imagenesArray = [
   },
 ];
 
+const body = document.querySelector('body');
+const boton1 = document.querySelector('#boton1');
+const boton2 = document.querySelector('#boton2');
+
 const fotoGranFragment = document.createDocumentFragment();
 const fotoGrande = document.querySelector('#fotoGrande');
 
 /* Eventos */
 
-<<<<<<< HEAD
-const body = document.querySelector('click', (ev) => {
-    if (ev.target.matches('#miniaturas')) {
-        // const agrandarMiniatura = 
-        // accededer al objeto por posición
-
-    };
-
-    // Aquí va función
-} 
-);
-=======
-const boton = document.getElementById('botones');
-const contenedorImagenes = document.getElementById('minuaturas');
-
-boton.addEventListener('click', function () {
-  if (contenedorImagenes.style.display === 'none') {
-    contenedorImagenes.style.display = 'block';
-    boton.textContent = 'Ocultar Fotos';
-  } else {
-    contenedorImagenes.style.display = 'none';
-    boton.textContent = 'Mostrar Fotos';
+// <<<<<<< HEAD
+body.addEventListener('click', (ev) => {
+  if (ev.target.matches('#boton1')) {
+    boton1.classList.add('o-m');
+    boton2.classList.remove('o-m');
+  } else if (ev.target.matches('#boton2')) {
+    boton2.classList.add('o-m');
+    boton1.classList.remove('o-m');
   }
-});
->>>>>>> db42997f7419c9b2cf8128c5df6d7d61d511e243
+});  
+
+//         // const agrandarMiniatura = 
+//         // accededer al objeto por posición
+
+//     };
+
+//     // Aquí va función
+// } 
+// );
+// =======
+// const boton = document.getElementById('botones');
+// const contenedorImagenes = document.getElementById('minuaturas');
+
+// boton.addEventListener('click', function () {
+//   if (contenedorImagenes.style.display === 'none') {
+//     contenedorImagenes.style.display = 'block';
+//     boton.textContent = 'Ocultar Fotos';
+//   } else {
+//     contenedorImagenes.style.display = 'none';
+//     boton.textContent = 'Mostrar Fotos';
+//   }
+// });
+// >>>>>>> db42997f7419c9b2cf8128c5df6d7d61d511e243
 
 /* Funciones */
 
