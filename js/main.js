61879cfe096cd374d6a09fdc32665ff12d3716bb
navarrorafa <115833775+navarrorafa@users.noreply.github.com>
2023-07-04 10:12:34 +0200

    rafa

--- js/main.js
+++ js/main.js
@@ -4,36 +4,42 @@ const miniFragmentAnadir = document.createDocumentFragment();
 const miniaturas = document.querySelector("#miniaturas");
 const imagenesArray = [
   {
+    id: 'foto1',
     url: 'assets/images/viajes-1.jpg',
     alt: 'Chica sobre hamaca en la playa',
     titulo: 'Viaje al Caribe',
     pie: 'Viaje uno',
   },
   {
+    id: 'foto2',
     url: 'assets/images/viajes-6.jpg',
     alt: 'Paseo maritimo con tunel natural',
     titulo: 'Viaje por acantilados',
     pie: 'Viaje dos',
   },
   {
+    id: 'foto3',
     url: 'assets/images/viajes-3.jpg',
     alt: 'Pueblo rural con castillo en el monte',
     titulo: 'Viaje trekking',
     pie: 'Viaje tres',
   },
   {
+    id: 'foto4',
     url: 'assets/images/viajes-2.jpg',
     alt: 'Chica sobre hamaca en la playa',
     titulo: 'Viaje al Caribe',
     pie: 'Viaje cuatro',
   },
   {
+    id: 'foto5',
     url: 'assets/images/viajes-5.jpg',
     alt: 'Paseo maritimo con tunel natural',
     titulo: 'Viaje por acantilados',
     pie: 'Viaje cinco',
   },
   {
+    id: 'foto6',
     url: 'assets/images/viajes-7.jpg',
     alt: 'Pueblo rural con castillo en el monte',
     titulo: 'Viaje trekking',
@@ -45,15 +51,11 @@ const imagenesArray = [
 const body = document.querySelector('body');
 const boton1 = document.querySelector('#boton1');
 const boton2 = document.querySelector('#boton2');
-
-
-
 const fotoGranFragment = document.createDocumentFragment();
 const fotoGrande = document.querySelector('#fotoGrande');
 
 // Eventos
 
-// Evento 1
 
 boton1.addEventListener('click', () => {
   boton1.style.display = 'none';
@@ -69,6 +71,13 @@ boton2.addEventListener('click', () => {
 
 // Evento 2
 
+imagenes.addEventListener('click' , (ev) => {
+  if(ev.target.id == id) {
+    imagenesArray.find((ev) => {
+      ev.
+    })
+  }
+})
 
 
 
@@ -84,67 +93,17 @@ boton2.addEventListener('click', () => {
 const cajasMiniaturas = () => {
   imagenesArray.forEach((item,index) => {
     const cajaMin = document.createElement("DIV");
-    cajaMin.classList.add('divImagen');
-    cajaMin.classList.add(`divImagen${index}`)
     const imagenes = document.createElement("IMG");
     imagenes.src = item.url, item.alt;
     cajaMin.append(imagenes);
+    cajaMin.id = item.id;
     miniFragmentAnadir.append(cajaMin);
   });
   miniaturas.append(miniFragmentAnadir);
 };
 
-const cajaFotosGran = () => {
-
-  imagenesArray.forEach((item,index) => {
-    const cajaGran = document.createElement('DIV');
-    cajaGran.classList.add('divImagenG');
-    cajaGran.classList.add(`divImagenG${index}`);
-    const cajaFig = document.createElement('FIGURE');
-    cajaFig.innerHTML = item.titulo;
-    cajaFig.classList.add('ampliar')
-    const imagenesGran = document.createElement('IMG');
-    imagenesGran.src = item.url, item.alt;
-    const cajaFigCap = document.createElement('FIGCAPTION');
-    cajaFigCap.innerHTML = item.pie;
-    imagenesGran.append(cajaFigCap);
-    cajaFig.append(imagenesGran);
-    cajaGran.append(cajaFig);
-    fotoGranFragment.append(cajaGran);
-  });
-  fotoGrande.append(fotoGranFragment);
-
-
-};
-
-
-
-/*
-const cajaFotosGran = () => {
-  imagenesArray.forEach((item) => {
-    const cajaGran = document.createElement('DIV');
-    const cajaFig = document.createElement('FIGURE',);
-    cajaFig.innerHTML = item.titulo;
-    const cajaFigCap = document.createElement('FIGCAPTION');
-    cajaFigCap.innerHTML = item.pie;
-    const imagenesGran = document.createElement('IMG');
-    imagenesGran.src = item.url, item.alt;
-    cajaFigCap.append(imagenesGran);
-    cajaFig.append(cajaFigCap);
-    cajaGran.append(cajaFig);
-    fotoGranFragment.append(cajaGran);
-  });
-  fotoGrande.append(fotoGranFragment);
-};
-*/
-
 
 cajasMiniaturas();
-cajaFotosGran();
 
-divImagen0.addEventListener('click', (ev) => {
-  divImagenG.style.display = 'none';
-  divImagenG0.style.display = 'block';
-  
-});
+
 
